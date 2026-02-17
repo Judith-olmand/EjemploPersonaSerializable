bdeca6bb6d5a9b874a20ee956cac30b94443e4e7
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-02-17 22:06:46 +0100

    acabado

--- src/LeerPersona.java
+++ src/LeerPersona.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class LeerPersona {
+    public static void main(String[] args) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("persona.ser"))) {
+            Persona persona = (Persona) ois.readObject();
+            System.out.println("Nombre: " + persona.getNombre() + ", Edad: " + persona.getEdad());
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error al leer el objeto: " + e.getMessage());
+        }
+    }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.io.Serializable;
 
-public class Persona {
+public class Persona implements Serializable {
+    private String nombre;
+    private int edad
+            ;
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
 }
